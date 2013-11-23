cdb1ea0bbeb9f598d5cfcb2f7f7bbe24a8b5fb78
theinternauts <cricketw@WeAreTheInternauts.com>
2013-11-22 20:40:52 -0800

    Added status flags and reader functions for the active state of a Bomb model

--- Bomb.js
+++ Bomb.js
@@ -1,13 +1,20 @@
 function Bomb( options ){
   if(!options){ options = {} }
+  var isActive = true;
   var blastRadius = options.blastRadius || 3
   var timer = options.timer || 3
   var id = options.id || Date.now()
   getTimer = function(){ return timer }
   getBlastRadius = function(){ return blastRadius }
   getID = function(){ return id }
+  getStatus = function(){
+    return {
+      isActive:isActive
+    }
+  }
 
   return {
-    id: getID
+    id: getID,
+    getStatus:getStatus
   }
 }
