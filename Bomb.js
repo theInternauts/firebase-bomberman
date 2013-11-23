7a6e412564d6b46eac8446cb91b7529e170a039e
Christoper Wallace <cricketw@weAreTheInternauts.com>
2013-11-23 02:50:28 -0800

    Updated Bomb model to allow for better reporting to the controller.  Created new utility functions to expose readers for some internal variables

--- Bomb.js
+++ Bomb.js
@@ -1,9 +1,15 @@
 function Bomb( options ){
-  if(!options){ options = {} }
-  var isActive = true;
+  var srcX = 200
+  var srcY = 600
+  var width = 35
+  var height = 35
+  var drawX = options.drawX
+  var drawY = options.drawY
+  var isActive = true
   var blastRadius = options.blastRadius || 3
   var timer = options.timer || 3
   var id = options.id || Date.now()
+  
   getTimer = function(){ return timer }
   getBlastRadius = function(){ return blastRadius }
   getID = function(){ return id }
@@ -13,8 +19,27 @@ function Bomb( options ){
     }
   }
 
+  getConfig = function(){
+    return {
+      srcX:srcX,
+      srcY:srcY,
+      width:width,
+      height:height
+    }
+  }
+
+  getPosition = function(){
+    return {
+      drawX: drawX,
+      drawY: drawY
+    }
+  }
+
   return {
     id: getID,
-    getStatus:getStatus
+    getTimer: getTimer,
+    getStatus: getStatus,
+    getConfig: getConfig,
+    getPosition: getPosition
   }
 }
